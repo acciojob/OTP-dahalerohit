d56dea7df350e6d915d6994789deb71b38e9d00f
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2025-10-06 22:34:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+let codes = document.querySelectorAll(".code")
+
+codes.forEach((code,index)=> {
+
+ code.addEventListener("input", () => {
+   if(code.value.length == 1){
+
+    if(index  < codes.length-1){
+
+        codes[index+1].focus()
+        codes[index+1].classList.add("focused")
+    }
+   }
+  
+ })
+
+ code.addEventListener("keydown", (e) => {
+
+    if(e.key == "Backspace"){
+
+       
+
+            if(index > 0 && code.value.length == 0)
+        
+             {
+                codes[index-1].focus()
+                codes[index-1].classList.add("focused")
+
+             }   
+        }
+    })
+
+    code.addEventListener("blur", () => {
+
+        code.classList.remove("focused")
+    })
+    
+
+ })
+
+ 
+
+
+
